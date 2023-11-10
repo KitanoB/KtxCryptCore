4040c36f71cef9c7ccc8749ff148c897ecbc1403
KitanoB <kitanoblog@proton.me>
2023-11-10 10:50:14 +0100

    Issue#4 Develop Unit Tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import kotlin.math.ceil
+
 val koin_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.10"
+    id("org.jetbrains.kotlinx.kover") version "0.7.4"
 }
 
 group = "com.kitano.crypto"
@@ -23,5 +26,43 @@ dependencies {
 kotlin {
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.register("updateCoverageBadge") {
+    doLast {
+
+        val coverageFile: Provider<RegularFile> = layout.buildDirectory.file("kover/coverage.txt")
+
+        if (!coverageFile.isPresent) {
+            println("Coverage file not found!")
+            return@doLast
+        }
+
+        val coverageText: String = coverageFile.get().asFile.readText()
+
+        val matchResult = Regex("""application line coverage: (\d+\.\d+)%""").find(coverageText)
+
+        if (matchResult == null) {
+            println("No match found in coverage.txt!")
+            return@doLast
+        }
+
+        val coveragePercentage = matchResult.groups[1]?.value ?: return@doLast
+
+        val readmeFile =  file("./README.md")
+
+        if (!readmeFile.exists()) {
+            println("README.md not found!")
+            return@doLast
+        }
+
+        var readmeContent = readmeFile.readText()
+        val roundedCoveragePercentage = ceil(coveragePercentage.toDouble()).toInt()
+        readmeContent = readmeContent.replace(Regex("\\!\\[Coverage]\\(https://img.shields.io/badge/Coverage-\\d+-green\\)"), "![Coverage](https://img.shields.io/badge/Coverage-${roundedCoveragePercentage}-green)")
+
+        readmeFile.writeText(readmeContent)
+
+        println("Code coverage badge updated and README.md updated!")
     }
 }
